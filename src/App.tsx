8c935d24e8873026edc5fc9c7639705c7f0ef7b4
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-10-04 19:28:33 +0300

    import inputs, outputs and shuffle them

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as tf from "@tensorflow/tfjs";
+import { TRAINING_DATA } from "./assets/real-estate-data";
+
+const INPUTS = TRAINING_DATA.inputs;
+const OUTUPTS = TRAINING_DATA.outputs;
+
+tf.util.shuffleCombo(INPUTS, OUTUPTS);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>hello world</>;
 }
 
-export default App
+export default App;
